32f3cf40b162c191e0fbddf5592795e87e6052bf
Gourav <gouravgunaga555@gmail.com>
2025-04-12 11:00:39 +0530

    chore:dummy data and types

---
+++ src/api/dummydata.ts
@@ -0,0 +1,136 @@
+import { THabitsResponse } from "./types/habits.types"
+
+const dummyData: THabitsResponse = [
+  {
+    id: "8eb2f878-6f7d-469f-99b3-a6854e6c7921",
+    user_id: "47a9af03-0619-40ba-b6cb-95ec54edc3a2",
+    name: "Coding",
+    description: null,
+    created_at: "2025-04-06T12:22:16.339822+00:00",
+    updated_at: "2025-04-06T12:22:16.339822+00:00",
+    entries: [
+      {
+        status: "DONE",
+        created_at: "2025-04-06T12:22:26.015801+00:00",
+      },
+      {
+        status: "DONE",
+        created_at: "2025-04-07T03:42:03.113501+00:00",
+      },
+      {
+        status: "DONE",
+        created_at: "2025-04-09T16:11:53.275745+00:00",
+      },
+      {
+        status: "DONE",
+        created_at: "2025-04-11T12:13:18.33916+00:00",
+      },
+    ],
+  },
+  {
+    id: "b2e28178-e5f7-4fd5-b266-68847201c04b",
+    user_id: "47a9af03-0619-40ba-b6cb-95ec54edc3a2",
+    name: "Walking",
+    description: null,
+    created_at: "2025-04-06T12:22:35.485326+00:00",
+    updated_at: "2025-04-06T12:22:35.485326+00:00",
+    entries: [
+      {
+        status: "NOTDONE",
+        created_at: "2025-04-06T12:22:50.531227+00:00",
+      },
+      {
+        status: "NOTDONE",
+        created_at: "2025-04-07T03:41:08.334235+00:00",
+      },
+      {
+        status: "NOTDONE",
+        created_at: "2025-04-08T04:26:46.266368+00:00",
+      },
+      {
+        status: "DONE",
+        created_at: "2025-04-09T06:24:31.898503+00:00",
+      },
+      {
+        status: "DONE",
+        created_at: "2025-04-10T04:21:25.140605+00:00",
+      },
+      {
+        status: "DONE",
+        created_at: "2025-04-11T12:12:42.40459+00:00",
+      },
+    ],
+  },
+  {
+    id: "25af6004-a803-496b-8ea8-e9e8e577d082",
+    user_id: "47a9af03-0619-40ba-b6cb-95ec54edc3a2",
+    name: "Cooking",
+    description: null,
+    created_at: "2025-04-06T12:22:58.533857+00:00",
+    updated_at: "2025-04-06T12:22:58.533857+00:00",
+    entries: [
+      {
+        status: "DONE",
+        created_at: "2025-04-06T12:23:12.965046+00:00",
+      },
+      {
+        status: "NOTDONE",
+        created_at: "2025-04-07T03:41:30.82135+00:00",
+      },
+      {
+        status: "NOTDONE",
+        created_at: "2025-04-08T04:27:13.018728+00:00",
+      },
+      {
+        status: "DONE",
+        created_at: "2025-04-09T06:24:55.939342+00:00",
+      },
+      {
+        status: "DONE",
+        created_at: "2025-04-10T04:21:56.14517+00:00",
+      },
+    ],
+  },
+  {
+    id: "e93d79bf-1ac0-4209-a862-65a06520f00b",
+    user_id: "47a9af03-0619-40ba-b6cb-95ec54edc3a2",
+    name: "Success",
+    description: null,
+    created_at: "2025-04-07T17:10:23.389556+00:00",
+    updated_at: "2025-04-07T17:10:23.389556+00:00",
+    entries: [
+      {
+        status: "DONE",
+        created_at: "2025-04-07T17:10:40.203113+00:00",
+      },
+      {
+        status: "DONE",
+        created_at: "2025-04-09T06:25:18.620907+00:00",
+      },
+      {
+        status: "DONE",
+        created_at: "2025-04-10T04:21:34.147301+00:00",
+      },
+      {
+        status: "NOTDONE",
+        created_at: "2025-04-11T12:12:57.753963+00:00",
+      },
+    ],
+  },
+  {
+    id: "d6372ae4-f2a7-406f-ad0d-13b52660eb4b",
+    user_id: "47a9af03-0619-40ba-b6cb-95ec54edc3a2",
+    name: "Wednesday ",
+    description: null,
+    created_at: "2025-04-09T06:25:35.467314+00:00",
+    updated_at: "2025-04-09T06:25:35.467314+00:00",
+    entries: [
+      {
+        status: "NOTDONE",
+        created_at: "2025-04-11T12:13:08.156634+00:00",
+      },
+    ],
+  },
+]
+
+export default dummyData
--- src/api/types/habits.types.ts
+++ src/api/types/habits.types.ts
@@ -10,3 +10,20 @@ export type TGetHabit = {
   name: string
   description: string | null
 }
+
+export type TEntry = {
+  status: "DONE" | "NOTDONE"
+  created_at: string
+}
+
+export type THabit = {
+  id: string
+  user_id: string
+  name: string
+  description: string | null
+  created_at: string
+  updated_at: string
+  entries: TEntry[]
+}
+
+export type THabitsResponse = THabit[]
